7a0e44b10eed782eea7978b7b86540e63d238200
Henry Kam <h3nrykam@gmail.com>
2021-10-20 15:15:16 -0700

    make port compatible with heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require('fs');
 const uniqid = require('uniqid'); 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const db = require('./db/db.json');
 
 const app = express();
